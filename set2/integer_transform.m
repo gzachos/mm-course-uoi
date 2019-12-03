a1c431760f8fe79c90101403b3884418f120e289
George Z. Zachos <gzachos@cse.uoi.gr>
2019-12-04 00:21:28 +0200

    Fixed indentation in starter files

--- set2/integer_transform.m
+++ set2/integer_transform.m
@@ -1,16 +1,12 @@
 function [W]= integer_transform(X)
+	% X is a 4x4 block of data
+	% W is the transformed coefficients
 
-% X is a 4x4 block of data
-% W is the transformed coefficients
+	% C is the core transform matrix
+	C =  [1  1  1  1
+	      2  1 -1 -2
+	      1 -1 -1  1
+	      1 -2  2 -1];
 
-
- % C is the core transform matrix
-C =  [1 1 1 1
-      2 1 -1 -2
-      1 -1 -1 1
-      1 -2 2 -1];
-
-  W = (C*X*C');
-
-
+	W = (C*X*C');
 end
--- set2/inv_integer_transform.m
+++ set2/inv_integer_transform.m
@@ -1,12 +1,9 @@
 function [Y] = inv_integer_transform(W)
+	% Ci is the inverse core transform matrix
+	Ci =  [1    1    1    1
+	       1    1/2 -1/2 -1
+	       1   -1   -1    1
+	       1/2 -1    1   -1/2];
 
-
- % Ci is the inverse core transform matrix
-Ci =  [1 1 1 1
-      1 1/2 -1/2 -1
-      1 -1 -1 1
-      1/2 -1 1 -1/2];
-
- Y = Ci'*W*Ci;
-
+	Y = Ci'*W*Ci;
 end

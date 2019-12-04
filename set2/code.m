e4811b25e819a4e66f71fb383345022729abc1bb
George Z. Zachos <gzachos@cse.uoi.gr>
2019-12-04 02:55:34 +0200

    Implemented part 3

--- set2/code.m
+++ set2/code.m
@@ -15,6 +15,10 @@ function print_usage()
 	printf("\t-s, --save-imgs        Save images\n");
 endfunction
 
+function retval = SAD(a,b)
+	retval = sum(sum(abs(a - b)));
+endfunction
+
 % The following options can be altered using command-line arguments
 save_imgs = 0;    % Save images option. Default value: False
 
@@ -41,7 +45,7 @@ frame179 = imread('frame179.tif');  % Read frame179 as a matrix
 rows178 = rows(frame178);
 cols178 = columns(frame178);
 
-% If images frame178 cannot be divided in 4x4 blocks, exit.
+% If image frame178 cannot be divided in 4x4 blocks, exit.
 if (mod(rows178,4) != 0 || mod(cols178,4) != 0)
 	printf("Image size is not multiple of 4!\n");
 	printf("Cannot partition frame178 to 4x4 blocks.\n");
@@ -104,4 +108,67 @@ if (save_imgs)
 	printf("\nSaving %s...\n\n", filename);
 	imwrite(i, filename);
 endif
+
+rows179 = rows(frame179);
+cols179 = columns(frame179);
+
+% If image frame179 cannot be divided in 16x16 blocks, exit.
+if (mod(rows179,16) != 0 || mod(cols179,16) != 0)
+	printf("Image size is not multiple of 16!\n");
+	printf("Cannot partition frame178 to 16x16 blocks.\n");
+	return;
+endif
+
+rowb = fix(rows179/16);      % Rows can be partitioned in rowb 16x16 blocks
+colb = fix(cols179/16);      % Columns can be partitioned in colb 16x16 blocks
+block_num = rowb * colb; % Total number of 16x16 blocks
+
+% Partition frame179 in 16x16 blocks
+row_block_sizes = ones(1,rowb) * 16;
+col_block_sizes = ones(1,colb) * 16;
+frame179_blocks = mat2cell(frame179, row_block_sizes, col_block_sizes);
+
+r = i;
+
+% For every 16x16 block:
+skips = 0;
+for k = 1:rowb
+	for l = 1:colb
+		b = frame179_blocks{k,l};   % Let b hold current 16x16 block
+		b = double(b); % Cast uint8 to double to avoid integer_transform complaining
+		xmin = 1 + (k-1)*16;
+		ymin = 1 + (l-1)*16;
+		xmax = k * 16;
+		ymax = l * 16;
+
+		a = r(xmin:xmax, ymin:ymax);
+		sad = SAD(a,b);
+		if (sad < 150)
+			skips++;
+			MVs{k,l} = [0,0];
+			continue;
+		endif
+
+		minSAD = {-1, [-10,-10]};
+		for i = -6:6
+			for j = -6:6
+				if (xmin+i < 1 || ymin+j < 1 ||
+						xmax+i > rows179 || ymax+j > cols179)
+					continue;
+				endif
+				a = r((xmin+i):(xmax+i), (ymin+j):(ymax+j));
+				sad = SAD(a,b);
+				if (minSAD{1} == -1 || sad < minSAD{1})
+					minSAD{1} = sad;
+					minSAD{2} = [i,j];
+				endif
+			endfor
+		endfor
+		MVs{k,l} = minSAD{2};
+	endfor
+endfor
+
+MVs
+
+printf('Blocks skipped: %2d\n', skips);
 

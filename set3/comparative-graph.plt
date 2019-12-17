55aff06f9f2dcac579f414608a31aeebd78fc922
George Z. Zachos <gzachos@cse.uoi.gr>
2019-12-17 14:21:55 +0200

    Updated set3 plots

--- set3/comparative-graph.plt
+++ set3/comparative-graph.plt
@@ -7,14 +7,14 @@ set xlabel "Bitrate (kbps)"
 set ylabel "Y-PSNR (dB)"
 set grid
 set colorbox vertical origin screen 0.9, 0.2, 0 size screen 0.05, 0.6, 0 front bdefault
-set style line 1 lc rgb '#3ab542' pt 7 lw 2 ps 1
-set style line 2 lc rgb '#ccab41' pt 7 lw 2 ps 1
-set style line 3 lc rgb '#ed4d4a' pt 7 lw 2 ps 1
-set style line 4 lc rgb '#407cc7' pt 7 lw 2 ps 1
+set style line 1 lc rgb '#ccab41' pt 11 lw 2 ps 1.5
+set style line 2 lc rgb '#407cc7' pt 13 lw 2 ps 1.5
+set style line 3 lc rgb '#ed4d4a' pt 9 lw 2 ps 1.5
+set style line 4 lc rgb '#3ab542' pt 3 lw 2 ps 1.5
 set key on inside bottom box
 INFILE='rp.dat'
-plot INFILE using 1:2 title 'H.264 IPP' with linespoints ls 2, \
-     INFILE using 3:4 title 'H.264 IBP' with linespoints ls 4, \
+plot INFILE using 1:2 title 'H.264 IPP' with linespoints ls 1, \
+     INFILE using 3:4 title 'H.264 IBP' with linespoints ls 2, \
      INFILE using 5:6 title 'H.265 Low-delay' with linespoints ls 3, \
-     INFILE using 7:8 title 'H.265 Random-access' with linespoints ls 1
+     INFILE using 7:8 title 'H.265 Random-access' with linespoints ls 4
 
--- set3/res.m
+++ set3/res.m
@@ -12,12 +12,15 @@ H265_P_LD = [37.5133, 39.68280, 41.26000, 42.46210];
 H265_R_RA = [98.6568, 147.4544, 196.3240, 245.0168];
 H265_P_RA = [37.7569, 39.87090, 41.47020, 42.69930];
 
-bjontegaard2(H264_R_B0, H264_P_B0, H264_R_B1, H264_P_B1, 'dsnr')
-bjontegaard2(H264_R_B0, H264_P_B0, H264_R_B1, H264_P_B1, 'rate')
+printf('# H.264 IPP vs H.264 IBP\n');
+BD_PSNR = bjontegaard2(H264_R_B0, H264_P_B0, H264_R_B1, H264_P_B1, 'dsnr')
+BD_Rate = bjontegaard2(H264_R_B0, H264_P_B0, H264_R_B1, H264_P_B1, 'rate')
 
-bjontegaard2(H264_R_B0, H264_P_B0, H265_R_LD, H265_P_LD, 'dsnr')
-bjontegaard2(H264_R_B0, H264_P_B0, H265_R_LD, H265_P_LD, 'rate')
+printf('\n# H.264 IPP vs H.265 Low-delay\n');
+BD_PSNR = bjontegaard2(H264_R_B0, H264_P_B0, H265_R_LD, H265_P_LD, 'dsnr')
+BD_Rate = bjontegaard2(H264_R_B0, H264_P_B0, H265_R_LD, H265_P_LD, 'rate')
 
-bjontegaard2(H264_R_B0, H264_P_B0, H265_R_RA, H265_P_RA, 'dsnr')
-bjontegaard2(H264_R_B0, H264_P_B0, H265_R_RA, H265_P_RA, 'rate')
+printf('\n# H.264 IPP vs H.264 Random-access\n');
+BD_PSNR = bjontegaard2(H264_R_B0, H264_P_B0, H265_R_RA, H265_P_RA, 'dsnr')
+BD_Rate = bjontegaard2(H264_R_B0, H264_P_B0, H265_R_RA, H265_P_RA, 'rate')
 
